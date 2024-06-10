0a9950ccaf5c5eb5ff28e3d72e06a58aec2612c9
yerin <81500522+YerinDoi@users.noreply.github.com>
2024-06-10 19:25:54 +0900

    Title: 체질량지수, Time: 151ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240610/체질량지수/bmi.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+
+        double h, w, b;
+        h = sc.nextInt();
+        w = sc.nextInt();
+        b = 10000 * w / (h * h);
+
+        System.out.printf("%d\n", (int)b);
+
+        if(b>=25) {
+            System.out.println("Obesity");
+        }
+
+    }
+}
