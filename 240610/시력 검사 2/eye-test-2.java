293065a1dfdd024b865cac6d4cdf4393d6207271
yerin <81500522+YerinDoi@users.noreply.github.com>
2024-06-10 23:37:09 +0900

    Title: 시력 검사 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 240610/시력 검사 2/eye-test-2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        double R = sc.nextDouble();
+
+        if(R >= 1.0) {
+            System.out.println("High");
+        }
+        esle if(R >= 0.5) {
+            System.out.println("Middle");
+        }
+        else {
+            System.out.println("Low");
+        }
+    }
+}
