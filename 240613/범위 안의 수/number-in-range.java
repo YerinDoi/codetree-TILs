3327cbe7f255a8f7c12bb1bc568cf736da850581
yerin <81500522+YerinDoi@users.noreply.github.com>
2024-06-13 23:52:03 +0900

    Title: 범위 안의 수, Time: 150ms, Memory: 10MB, Status: Passed - CodeTree

--- 240613/범위 안의 수/number-in-range.java
+++ 240613/범위 안의 수/number-in-range.java
@@ -10,6 +10,6 @@ public class Main {
         if(a>=10 && a<=20)
             System.out.println("yes");
         else
-            System.out.println("no")
+            System.out.println("no");
     }
 }
