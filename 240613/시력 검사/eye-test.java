60159ded18986150e9cc2c1fac8c9aae8fab9092
yerin <81500522+YerinDoi@users.noreply.github.com>
2024-06-13 23:55:43 +0900

    Title: 시력 검사, Time: 137ms, Memory: 11MB, Status: Passed - CodeTree

--- 240613/시력 검사/eye-test.java
+++ 240613/시력 검사/eye-test.java
@@ -8,7 +8,9 @@ public class Main {
         double R = sc.nextDouble();
         double L = sc.nextDouble();
 
-        if(R >= 1.0 && L >= 20)
+        if(R >= 1.0 && L >= 1.0)
+            System.out.println("High");
+        else if(R >= 0.5 && L >= 0.5)
             System.out.println("Middle");
         else
             System.out.println("Low");
