4d4db93bca970332056c35128add2978e161f925
yerin <81500522+YerinDoi@users.noreply.github.com>
2024-06-14 19:39:38 +0900

    Title: 일 수 구하기, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 240614/일 수 구하기/number-of-days-in-month.java
+++ 240614/일 수 구하기/number-of-days-in-month.java
@@ -3,19 +3,23 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
 
-        if(n==2) {
-            System.out.println(28);
-        }
-        else if (n <= 7){
-            if(n % 2 == 1){
+        // 변수 선언
+        int n;
+        
+        // 입력
+        n = sc.nextInt();
+
+        // 출력
+        if(n == 2)
+            System.out.println("28");
+        else if(n <= 7) {
+            if(n % 2 == 1)
                 System.out.println("31");
-            }
             else
-                System.out.println("20");
+                System.out.println("30");
         }
-        else{
+        else {
             if(n % 2 == 0)
                 System.out.println("31");
             else
