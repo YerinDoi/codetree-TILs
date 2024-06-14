bde8bc3f14297ad88f200939d07b2eae32ecd149
yerin <81500522+YerinDoi@users.noreply.github.com>
2024-06-15 01:11:24 +0900

    Title: 중앙값 구하기, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240615/중앙값 구하기/find-the-median.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b, c;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+
+        if(a > b) {
+            if(b > c)
+                System.out.println(b);
+            else {
+                if(a > c)
+                    System.out.println(c);
+                else
+                    System.out.println(a);
+            }
+
+        }
+        else {
+            if(a > c)
+                System.out.println(a);
+            else {
+                if(b > c)
+                    System.out.println(c);
+                else
+                    System.out.println(b);
+            }
+        }
+    }
+}
