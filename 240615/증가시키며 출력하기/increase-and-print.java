26fe0a57d22aab8f8c40d91695248535477463a2
yerin <81500522+YerinDoi@users.noreply.github.com>
2024-06-15 23:50:29 +0900

    Title: 증가시키며 출력하기, Time: 86ms, Memory: 8MB, Status: Wrong Answer - CodeTree

--- 240615/증가시키며 출력하기/increase-and-print.java
+++ 240615/증가시키며 출력하기/increase-and-print.java
@@ -1,9 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
-        Scanner sc = new Scanner(Ssytem.in);
 
         for(int i = 5; i <18; i += 2) {
             System.out.println(i);
