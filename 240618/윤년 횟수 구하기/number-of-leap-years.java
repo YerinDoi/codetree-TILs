97f8d4b2aa84794e68d8f7d78cb0788f531b60db
yerin <81500522+YerinDoi@users.noreply.github.com>
2024-06-18 00:31:48 +0900

    Title: 윤년 횟수 구하기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240618/윤년 횟수 구하기/number-of-leap-years.java
+++ 240618/윤년 횟수 구하기/number-of-leap-years.java
@@ -8,7 +8,7 @@ public class Main {
         int cnt = 0;
 
         for(int i = 1; i <= n; i++) {
-            if(i % 4 == 0 && i % 400 != 0)
+            if((i % 4 == 0 && i % 100 != 0) || i % 400 == 0)
                 cnt++;
         }
         System.out.println(cnt);
