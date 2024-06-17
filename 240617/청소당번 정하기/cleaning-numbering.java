f8f97c03c687f40682aa5218319e9e6e1d3e761b
yerin <81500522+YerinDoi@users.noreply.github.com>
2024-06-17 23:47:51 +0900

    Title: 청소당번 정하기, Time: 131ms, Memory: 10MB, Status: Passed - CodeTree

--- 240617/청소당번 정하기/cleaning-numbering.java
+++ 240617/청소당번 정하기/cleaning-numbering.java
@@ -5,19 +5,16 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int cnt1 = 0;
-        int cnt2 = 0;
-        int cnt3 = 0;
+        int cnt2 = 0, cnt3 = 0, cnt12 = 0;
 
-        for(int i = 0; i < n; i++) {
-            if(i % 2 == 0 && i % 6 != 0 && i % 12 != 0)
-                // 교실 청소
-                cnt1++;
-            else if(i % 3 == 0 && i % 12 != 0)
-                cnt2++;
-            else if(i % 12 == 0)
+        for(int i = 1; i <= n; i++) {
+            if(i % 12 == 0)
+                cnt12++;
+            else if(i % 3 == 0)
                 cnt3++;
+            else if(i % 2 == 0)
+                cnt2++;
         }
-        System.out.println(cnt1 + " " + cnt2 + " " + cnt3);
+        System.out.println(cnt2 + " " + cnt3 + " " + cnt12);
     }
 }
