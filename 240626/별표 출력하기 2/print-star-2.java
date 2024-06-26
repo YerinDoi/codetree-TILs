20b6aacd650cb51776d6a407887a6e0fa8a7078f
yerin <81500522+YerinDoi@users.noreply.github.com>
2024-06-26 23:35:48 +0900

    Title: 별표 출력하기 2, Time: 132ms, Memory: 10MB, Status: Passed - CodeTree

--- 240626/별표 출력하기 2/print-star-2.java
+++ 240626/별표 출력하기 2/print-star-2.java
@@ -6,9 +6,9 @@ public class Main {
          
         int n = sc.nextInt();
 
-        for(int i = n; i > 0; i--) {
-            for(int j = i; j <= i; j--) {
-                System.out.print("*");
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n - i; j++) {
+                System.out.print("* ");
             }
             System.out.println();
         }
